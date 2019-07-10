953f4aa1ce0b5cc02757040a5a113f79d1dee35e
Sean Belverstone <seanbelverstone@gmail.com>
2019-07-10 16:18:04 -0500

    Added the snapshot and all the math to work out the next train time and also its minutes away. Added the appending data cells and row so it adds the data to the table. Also added in the current time to help with my math

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -24,11 +24,13 @@ var firebaseConfig = {
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
+    //Grabs the data from the form
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
-    var firstTrain = $("#firstTrainTimeInput").val().trim();
+    var firstTrain = moment($("#firstTrainTimeInput").val().trim(), "HH:mm").format("HH:mm");
     var frequency = $("#frequencyInput").val().trim();
 
+    //Stores it in a JSON object called newTrain
     var newTrain = {
         name: trainName,
         destination: destination,
@@ -36,10 +38,51 @@ $("#submit").on("click", function(event) {
         frequency: frequency
     };
 
+    //Push newTrain to Firebase
     database.ref().push(newTrain);
     console.log(newTrain);
 
-    
+    alert("New train added!");
+
+    //Clears the form
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#destinationInput").val("");
+    $("#frequencyInput").val("");
+
+});
+
+//Firebase event
+database.ref().on("child_added", function(childSnapshot) {
+
+//Storing the firebase snapshot into a variable
+var trainName = childSnapshot.val().name;
+var destination = childSnapshot.val().destination;
+var firstTrain = childSnapshot.val().firstTrain;
+var frequency = childSnapshot.val().frequency;
+      
+//current time
+var currentTime = moment();
+
+//This subtracts the first train by a year to make sure it's before the current time (moment)
+var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+// This calculates the difference between the current time and the first train
+var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+var remainder = difference % frequency;
+var minutesAway = frequency - remainder;
+var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
+
+//create the new rows and data cells
+var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(nextArrival),
+    $("<td>").text(minutesAway),
+  );
+
+  //append to the table
+  $("#trainTable > tbody").append(newRow);
 
 
 });

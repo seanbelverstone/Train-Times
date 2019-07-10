548ce7b3ab2c08140773cea25778d54827561511
Sean Belverstone <seanbelverstone@gmail.com>
2019-07-10 15:15:58 -0500

    Customised the styling a little more and added psuedocode on javascript

---
+++ assets/javascript/logic.js
@@ -0,0 +1,6 @@
+/* Pseudocode
+When user presses submit button, update the HTML with their new train and upload to firebase.
+Retrieve the new trains from firebase.
+Calculate the next arrival against first train time
+Calculate minutes away
+*/
